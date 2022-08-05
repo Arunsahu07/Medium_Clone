c95d81e3b279859fe397b99e03565875072da745
Arunsahu07 <83357476+Arunsahu07@users.noreply.github.com>
2022-08-05 09:29:21 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,5 @@ const nextConfig = {
   // presets : ["next/babel"]
 }
 
-// module.exports = nextConfig
+module.exports = nextConfig
 // // "presets": ["next/babel"]
-module.exports = {
-  distDir: 'build',
-}
