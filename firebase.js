9906ade76663da77fbed681016ba1a5e476f98a8
Arunsahu07 <83357476+Arunsahu07@users.noreply.github.com>
2023-03-08 18:24:42 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,10 +4,15 @@ import {getAuth } from "firebase/auth";
 import {GoogleAuthProvider } from "firebase/auth";
 import {getFirestore } from "firebase/firestore";
 // import dotenv from "dotenv"
- {REACT_APP_API_KEY,  REACT_APP_PROJECT_ID,REACT_APP_STORAGE_BUCKET, 
-  REACT_APP_AUTH_DOMAIN,REACT_APP_MESSAGING_SENDER_ID, REACT_APP_APP_ID} = process.env
+//  {REACT_APP_API_KEY,  REACT_APP_PROJECT_ID,REACT_APP_STORAGE_BUCKET, 
+//   REACT_APP_AUTH_DOMAIN,REACT_APP_MESSAGING_SENDER_ID, REACT_APP_APP_ID} = process.env
 
-
+  const  REACT_APP_API_KEY = "AIzaSyDbqo0X-CRgf9yb8Zekg8vYd0EUTeLUFko"
+  const  REACT_APP_PROJECT_ID = "my-medium-7f7aa"
+  const  REACT_APP_STORAGE_BUCKET =  "my-medium-7f7aa.appspot.com"
+  const  REACT_APP_AUTH_DOMAIN = "my-medium-7f7aa.firebaseapp.com"
+  const  REACT_APP_MESSAGING_SENDER_ID = "900833332845"
+  const  REACT_APP_APP_ID = "1:900833332845:web:2dd4016689e734d6f13964"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
